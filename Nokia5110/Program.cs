9b0b089fc7d86a204bcd0d8603fb2b7bfdf85df9
Wilco de Jong <wilco@willybilly.nl>
2018-12-22 12:07:33 +0100

    First commit

---
+++ Nokia5110/Program.cs
@@ -0,0 +1,19 @@
+﻿using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Nokia5110
+{
+    public class Program
+    {
+        public static Nokia.LCDScreens.Nokia_5110 Lcd = new Nokia.LCDScreens.Nokia_5110(true, Pins.GPIO_PIN_D4, PWMChannels.PWM_PIN_D10, Pins.GPIO_PIN_D5, Pins.GPIO_PIN_D3);
+
+        public static void Main()
+        {
+            Lcd.Clear();
+            Lcd.WriteText("willybilly.nl", false);
+            Lcd.WriteText("               ", false);
+            Lcd.WriteText("github.com/   ", false);
+            Lcd.WriteText("willybilly90", false);
+        }
+
+    }
+}
